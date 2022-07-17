9b031fcae38e7a351356634ee6e006019346693e
Bharat Patodi <bharatpatodi@gmail.com>
2022-07-17 16:42:30 +0000

    Add up and down lift logic

--- src/js/main.js
+++ src/js/main.js
@@ -14,8 +14,8 @@ addFloorBtn.addEventListener("click", () => {
           <hr>
           <div class="floor-main-part">
             <div class="floor-btn">
-              <button class="floor-btn-up">Up</button>
-              <button class="floor-btn-down">Down</button>
+              <button class="floor-btn-up" onclick = "moveLiftUp(event)">Up</button>
+              <button class="floor-btn-down" onclick = "moveLiftDown(event)">Down</button>
             </div>
 
             <!-- Lift section -->
@@ -52,3 +52,38 @@ addLiftBtn.addEventListener("click", () => {
   lastFloor.getElementsByClassName("lifts")[0].appendChild(lift);
 
 });
+
+// Move lift up logic
+
+let moveLiftUp = (e) => {
+    console.log("move lift up");
+    let lift = e.target.parentElement.parentElement.parentElement.getElementsByClassName("lift")[0];
+    let floor = e.target.parentElement.parentElement.parentElement;
+    let floors = document.getElementsByClassName("floor");
+    let lastFloor = floors[floors.length - 1];
+    let lastFloorLift = lastFloor.getElementsByClassName("lift")[0];
+    if (floor === lastFloor) {
+        lastFloorLift.remove();
+        floor.getElementsByClassName("lifts")[0].appendChild(lift);
+    } else {
+        let nextFloor = floor.nextElementSibling;
+        nextFloor.getElementsByClassName("lifts")[0].appendChild(lift);
+    }
+}
+
+// Move lift down logic
+let moveLiftDown = (e) => {
+    console.log("move lift down");
+    let lift = e.target.parentElement.parentElement.parentElement.getElementsByClassName("lift")[0];
+    let floor = e.target.parentElement.parentElement.parentElement;
+    let floors = document.getElementsByClassName("floor");
+    let firstFloor = floors[0];
+    let firstFloorLift = firstFloor.getElementsByClassName("lift")[0];
+    if (floor === firstFloor) {
+        firstFloorLift.remove();
+        floor.getElementsByClassName("lifts")[0].appendChild(lift);
+    } else {
+        let prevFloor = floor.previousElementSibling;
+        prevFloor.getElementsByClassName("lifts")[0].appendChild(lift);
+    }
+}
